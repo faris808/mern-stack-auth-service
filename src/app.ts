e40464eb101326641a0f9269ef9ab25f7f8a0521
faris808 <fariszama2019@gmail.com>
2024-11-17 16:29:19 +0530

    add test for persist user

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
+import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
 import logger from "./config/logger";
 import { HttpError } from "http-errors";
 import authRouter from "./routes/auth";
-import "reflect-metadata";
 
 const app = express();
 
-app.get("/", async (req, res) => {
+app.use(express.json());
+
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+app.get("/", (req, res) => {
     res.send("Welcome to auth service and hello world and hello everyone");
 });
 
 app.use("/auth", authRouter);
 
+//Ye ensure karna hai ki ye sab se last mein aaye tab hi saare error ko ye catch kar payega
+//Global error handler
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
     logger.error(err.message);
