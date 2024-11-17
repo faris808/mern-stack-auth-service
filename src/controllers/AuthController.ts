d800f111b17e898d6205e14cbcd819e2a423e4ae
faris808 <fariszama2019@gmail.com>
2024-11-18 01:04:36 +0530

    Email require validation

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Response } from "express";
 import { RegisterUserRequest } from "../types";
 import { UserService } from "../services/UserService";
 import { Logger } from "winston";
+import { validationResult } from "express-validator";
 
 export class AuthController {
     constructor(
@@ -13,6 +14,12 @@ export class AuthController {
         res: Response,
         next: NextFunction,
     ) {
+        // Validation
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.status(400).json({ errors: result.array() });
+        }
+
         const { firstName, lastName, email, password } = req.body;
         this.logger.debug("New request to register a user", {
             firstName,
@@ -29,8 +36,8 @@ export class AuthController {
             });
             this.logger.info("User has been registered", { id: user.id });
             res.status(201).json({ id: user.id });
-        } catch (error) {
-            next(error);
+        } catch (err) {
+            next(err);
             return;
         }
     }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,17 +1,21 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { AuthController } from "../controllers/AuthController";
 import { UserService } from "../services/UserService";
 import { AppDataSource } from "../config/data-source";
 import { User } from "../entity/User";
 import logger from "../config/logger";
+import registerValidator from "../validators/register-validator";
 const router = express.Router();
 
 const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository); //This is only called dependency injection
 //Dependency Injection :- Jo bhi dependency aapko chahiye hoti hai apki class ke liye usko aap constructor ke andar se receive karte ho
 const authController = new AuthController(userService, logger);
-router.post("/register", (req, res, next) =>
-    authController.register(req, res, next),
+router.post(
+    "/register",
+    registerValidator,
+    (req: Request, res: Response, next: NextFunction) =>
+        authController.register(req, res, next),
 );
 
 export default router;
