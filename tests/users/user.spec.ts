066a386ce9128b91c19f61c49755e786dca2b348
faris808 <fariszama2019@gmail.com>
2024-11-20 18:49:53 +0530

    add test to exclude password field

--- tests/users/user.spec.ts
+++ tests/users/user.spec.ts
@@ -71,5 +71,37 @@ describe("GET /auth/self", () => {
             //Check if user id matches with registered user
             expect((response.body as Record<string, string>).id).toBe(data.id);
         });
+
+        it("should not return the password field", async () => {
+            //Register a user
+            const userData = {
+                firstName: "Faris",
+                lastName: "Z",
+                email: "test@mern.space",
+                password: "secreteee",
+            };
+            const userRepository = connection.getRepository(User);
+            const data = await userRepository.save({
+                ...userData,
+                role: Roles.CUSTOMER,
+            });
+            //Generate token
+            const accessToken = jwks.token({
+                sub: String(data.id),
+                role: data.role,
+            });
+
+            //Add token to cookie
+            const response = await request(app)
+                .get("/auth/self")
+                .set("Cookie", [`accessToken=${accessToken};`])
+                .send();
+            expect(response.statusCode).toBe(200);
+            //Assert
+            //Check if user id matches with registered user
+            expect(response.body as Record<string, string>).not.toHaveProperty(
+                "password",
+            );
+        });
     });
 });
